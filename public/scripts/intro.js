e82de63bc44515fa790b7f28ec34608670860b07
gabriel-fogaca <67604400+gabriel-fogaca@users.noreply.github.com>
2021-03-31 12:15:16 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,66 @@
+//comments
+//document.write("Hello")
+
+// variaveis, tipos de dados
+//let myvar = "Hello"
+//const myconst = "He"
+
+//document.write(myconst + myvar)
+
+// string
+// const s1 = "Isso é uma sting"
+// const s2 = 'Isso também é um string'
+// const s3 = 'Isso é uma string'
+
+// document.write()
+
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+//objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande lógica de correr")
+//   }
+// }
+// pessoa.correr()
+
+// Array - vetores
+// const colecaoDeBolinhas  = ["blue", "green", 1, {name: "my name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+// function sayMyName(name){
+//   document.write(name)
+// 
+
+// //executar
+// sayMyName("Douglas ")
+// sayMyName("Valeska ")
+// sayMyName("Kyam")
+
+//condicionais
+
+// const notaFinal = 8
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+//loop - repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p> ${i}</p>`)
+// }
